07c37e34fd587afc4794b260b83728984b614963
Gabriel-Medeiros-faria <gabrielmedeirosvino@gmail.com>
2025-08-27 00:04:02 -0300

    feat: button comprar

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -463,7 +463,7 @@ const ServiceCard = ({
               </>
             ) : (
               <>
-                Selecionar Plano
+                Comprar
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
